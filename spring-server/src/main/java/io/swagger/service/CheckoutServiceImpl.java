8efdc8591336379487584a47f49c08dd9fb07737
unknown <stefan.tomasik@synapse.com>
2020-01-10 10:00:54 +0100

    added funcionality

--- spring-server/src/main/java/io/swagger/service/CheckoutServiceImpl.java
+++ spring-server/src/main/java/io/swagger/service/CheckoutServiceImpl.java
@@ -5,9 +5,15 @@
  */
 package io.swagger.service;
 
+import com.sovy.models.Book;
 import io.swagger.model.Pet;
 import io.swagger.repository.CheckoutRepository;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
 import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,30 +23,67 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class CheckoutServiceImpl implements CheckoutService {
-    
+
     @Autowired
     private CheckoutRepository service;
-    
+
+    PomDatabase database = new PomDatabase();
+
     @Override
     public Pet addCheckout(Pet checkout) {
-        return service.save(checkout);             
+        return service.save(checkout);
     }
-    
+
     @Override
     public Pet updateCheckout(Pet checkout) {
         checkout = service.findById(checkout.getId()).get();
         return service.save(checkout);
     }
-    
+
     @Override
     public Pet getCheckoutById(long id) {
-         Optional<Pet> pet = service.findById(id);
-        return pet.get();
-    }
+        List idUser = null;
+        List idBook = null;  
+        Pet petObject = null;
+        try {
+            idUser = database.getUserById();
+            idBook = database.getBookById();
+
+            // int sizeUser=idUser.size();
+            //int sizeBook=idBook.size();
+        } catch (IOException ex) {
+            Logger.getLogger(CheckoutServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(CheckoutServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(CheckoutServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        Pet pet = service.findById(id).get();
+        int index1 = 0;
+        for (int i = 0; i < idUser.size(); i++) {
+            if (idUser.get(i).equals(pet.getIdUser())) {
+                index1 = i;
+            }
+        }
+
+        int index2 = 0;
+        for (int i = 0; i < idBook.size(); i++) {
+            if (idBook.get(i).equals(pet.getIdBook())) {
+                index2 = i;
+            }
+        }
+
+        if ((index1 >= 0) && (index2 >= 0)) {
+            petObject = pet;
+        }
     
+        return petObject;
+    }
+
     @Override
     public void deleteCheckout(long id) {
-    service.deleteById(id);
+        service.deleteById(id);
     }
-    
+
 }
---
+++ spring-server/src/main/java/io/swagger/service/PomDatabase.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.swagger.service;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ *
+ * @author stefan.tomasik
+ */
+public class PomDatabase {
+
+    public Connection createConnection() throws SQLException, ClassNotFoundException, FileNotFoundException, IOException {
+        Connection connection;
+        Properties properties = new Properties();
+        properties.load(new java.io.FileInputStream("C://Users//stefan.tomasik//Documents//NetBeansProjects//Review//src//main//java//com//sovy//review//application.properties"));
+        String url = properties.getProperty("host");
+        String user = properties.getProperty("username");
+        String password = properties.getProperty("password");
+        String driver = properties.getProperty("driver");
+        Class.forName(driver);
+
+        connection = DriverManager.getConnection(url, user, password);
+        return connection;
+    }
+
+    public List getBookById( ) throws IOException, SQLException, FileNotFoundException, ClassNotFoundException {
+        Long idKniha = null;
+        List<Long> list = new ArrayList();
+        String dopyt = "SELECT id from Book where id>0";
+        Connection connectionBook = createConnection();
+        PreparedStatement preparedStatementBook = connectionBook.prepareStatement(dopyt);
+        ResultSet rss = preparedStatementBook.executeQuery();
+
+        while (rss.next()) {
+            idKniha = rss.getLong(1);
+            list.add(idKniha);
+        }
+        connectionBook.close();
+        return list;
+
+    }
+    
+    
+        public List getUserById( ) throws IOException, SQLException, FileNotFoundException, ClassNotFoundException {
+        Long idUser = null;
+        List<Long> list = new ArrayList();
+        String dopyt = "SELECT id from Pouzivatel where id>0";
+        Connection connectionUser= createConnection();
+        PreparedStatement preparedStatementBook = connectionUser.prepareStatement(dopyt);
+        ResultSet rss = preparedStatementBook.executeQuery();
+
+        while (rss.next()) {
+            idUser = rss.getLong(1);
+            list.add(idUser);
+        }
+        connectionUser.close();
+        return list;
+
+    }
+
+}
